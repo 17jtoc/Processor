cf2a59beb29db11388c8041429b92a8f7e32f0e1
17jtoc <37631024+17jtoc@users.noreply.github.com>
2020-03-01 17:58:58 -0500

    shrunk it

--- divide.vhd
+++ divide.vhd
@@ -12,44 +12,29 @@ ENTITY divide IS
 END divide;
 
 ARCHITECTURE behavioral OF divide IS
-
-
-
 BEGIN
 	PROCESS(dividend,divisor)
 	variable A : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	variable M : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	variable Q : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	variable Res : STD_LOGIC_VECTOR(31 DOWNTO 0); 
-	
 		BEGIN
-			
 			A := "00000000000000000000000000000000";
 			M := divisor;
 			Q := dividend;
-			
-			
 			FOR n IN 0 TO 31 LOOP
 				A := A(30 DOWNTO 0) & Q(31);
 				Q := Q(30 DOWNTO 0) & '0';
 				Res := A;
 				A := A - M;
-				
 				if A(31) = '0' then
-					Q(0) := '1';
-					
+					Q(0) := '1';	
 				else
 					Q(0) := '0';
 					A := Res;
-				end if;
-					
-				
 			END LOOP;
-			
 			Output <= Q;
 			rOut <= A;
-			
 		END PROCESS;
-	
 END behavioral;
 	
